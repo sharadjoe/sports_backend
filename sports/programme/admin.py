0f3e54b0f4dadafd0b9f3905433a83f571e97e5f
matthewzalex <matthewzalex@gmail.com>
2018-03-15 20:04:04 +0530

    Working. doneprogramme entry. add?yes! delete?no!

--- sports/programme/admin.py
+++ sports/programme/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Programme)
+admin.site.register(models.Event)
+admin.site.register(models.House)
+admin.site.register(models.Student)
--- sports/programme/forms.py
+++ sports/programme/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from django.db import models
+from .models import Programme, Event, House, Student
 
 class ProgrammeForm(ModelForm):
     class Meta:
